440162976e462850501b59d6a1d89460fee0a71f
HIRO_F <takuju624@gmail.com>
2025-01-30 03:07:50 +0900

    add

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/vocabularyapp/ui/screens/ResultScreenKt.java
@@ -0,0 +1,15 @@
+package com.example.vocabularyapp.ui.screens;
+
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Alignment;
+import androidx.compose.ui.Modifier;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0018\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a4\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00032\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00010\u00062\f\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\u00010\u0006H\u0007\u00a8\u0006\b"}, d2 = {"ResultScreen", "", "correctAnswers", "", "totalQuestions", "onRetry", "Lkotlin/Function0;", "onBackToHome", "app_debug"})
+public final class ResultScreenKt {
+    
+    @androidx.compose.runtime.Composable()
+    public static final void ResultScreen(int correctAnswers, int totalQuestions, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onRetry, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onBackToHome) {
+    }
+}
